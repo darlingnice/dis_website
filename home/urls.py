40ce352a214d2f4f58e19d8bf371dc86f3a8d7a7
darlingnice <uromdarlington@gmail.com>
2025-05-03 03:04:21 +0100

    Modified the whole templates

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
+    path('about/',views.about,name="about"),
+    path('classes/',views.classes,name="classes"), 
+    path('facility/',views.facility,name="facility"),
+    path('become-a-tearcher/',views.call_to_action,name="call-to-action"),   
+    path('appointment/',views.appointment,name="appointment"), 
+    path('testimonials/',views.testimonials,name="testimonials"),  
+    path('contact/',views.contact,name="contact"),    
+
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,29 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"home.html",{})
+
+
+
+def about(request):
+    return render(request,"about.html",{})
+
+def classes(request):
+    return render(request,"classes.html",{})
+
+
+def facility(request):
+    return render(request,"facility.html",{})
+
+
+def call_to_action(request):
+    return render(request,"call-to-action.html",{})
+
+def appointment(request):
+    return render(request,"appointment.html",{})
+
+def testimonials(request):
+    return render(request,"testimonials.html",{})
+
+
+def contact(request):
+    return render(request,"contact.html",{})
